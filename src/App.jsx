bb68d373c46591df5347d930192743ab94225fa3
Abhishek7356 <ksabhishek32@gmail.com>
2024-02-09 15:20:34 +0530

    updated to folder structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,23 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Cart from './pages/Cart';
+import Product from './pages/Product';
+import Products from './pages/Products';
+import Wishlist from './pages/Wishlist';
+import Header from './components/Header';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div>
+      <Header />
       <Routes>
-        
+        <Route path='/product/:id' element={<Product />} />
+        <Route path='/products' element={<Products />} />
+        <Route path='/wishlist' element={<Wishlist />} />
+        <Route path='/cartlist' element={<Cart />} />
       </Routes>
+      <Footer />
     </div>
   );
 }
