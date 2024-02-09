ffea7b968e23f564b266185e064c48a2dd5d37cc
Abhishek7356 <ksabhishek32@gmail.com>
2024-02-09 15:06:37 +0530

    made a small change added folders for pages and components

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
     <div>
-      <h1>E-commerse website</h1>
+      <Routes>
+        
+      </Routes>
     </div>
   );
 }
