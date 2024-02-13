d2158c9e0ba74c62d26836653085aa6c502d3f29
Midhunkm17 <you@example.com>
2024-02-13 13:03:47 +0530

    acc_wishlist

--- src/pages/AccountWishlist.jsx
+++ src/pages/AccountWishlist.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import { Box } from '@mui/material'
+import Wishlist from './Wishlist'
 
-const AccountWishlist = () => {
-    return (
-        <div>AccountWishlist</div>
-    )
+function AccountWishlist() {
+  return (
+    <Box>
+        <Wishlist Wishlist/>
+    </Box>
+  )
 }
 
 export default AccountWishlist
