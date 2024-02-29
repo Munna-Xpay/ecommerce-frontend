4bc2f02d1449a1c0ccb03d4985ade3729e21da38
Abhishek7356 <ksabhishek32@gmail.com>
2024-02-29 12:33:28 +0530

    updated cart page

--- src/components/ProductsTable.jsx
+++ src/components/ProductsTable.jsx
@@ -17,24 +17,17 @@ import {
 import DeleteIcon from "@mui/icons-material/Delete";
 import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
 import { useDispatch, useSelector } from "react-redux";
-import { deleteCartItem } from "../redux/cartSlice";
+import { deleteCartItem, updateCartItem } from "../redux/cartSlice";
 
-function ProductsTable({ isWishlist }) {
+function ProductsTable({ isWishlist, products }) {
 
   const dispatch = useDispatch()
-  const cartitems = useSelector(state => state.cartReducer.cartItems)
-
-  const [qty, setQty] = useState('');
-
-  const handleChange = (event) => {
-    setQty(event.target.value);
-  };
 
   const handleCartItemDelete = (id) => {
     dispatch(deleteCartItem(id))
   }
 
-  const showCartItems = cartitems.map((item, index) => {
+  const showCartItems = products.map((item, index) => {
     return (
       <TableRow
         sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
@@ -65,14 +58,15 @@ function ProductsTable({ isWishlist }) {
           <FormControl sx={{ ms: 2, minWidth: 50 }} size="small">
             <Select
               defaultValue={1}
-              onChange={handleChange}
+              onChange={(e) => dispatch(updateCartItem({ id: item._id, data: { quantity: e.target.value, original_price: item.product.original_price } }))}
+              value={item.quantity}
               sx={{ height: '33px' }}
             >
-              <MenuItem value="">
-              </MenuItem>
-              <MenuItem selected={true} value={1}>1</MenuItem>
+              <MenuItem value={1}>1</MenuItem>
               <MenuItem value={2}>2</MenuItem>
               <MenuItem value={3}>3</MenuItem>
+              <MenuItem value={4}>4</MenuItem>
+              <MenuItem value={5}>5</MenuItem>
             </Select>
           </FormControl>
         </TableCell>
